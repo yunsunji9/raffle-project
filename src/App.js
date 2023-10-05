d46ecc433e639887d461fbc77e3f8a55a65b7bd2
yunseonji <amy@shoeprize.com>
2023-10-05 15:07:19 +0900

    타이틀 영역 추가

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header" style={{border: '1px solid red'}}>
+        <div className="title">title 입니다.</div>
         <p>
           안녕하세요 윤선지입니다.
         </p>
