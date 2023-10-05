a8210e853da06cfd5c41725d6e537f59901c608a
yunseonji <amy@shoeprize.com>
2023-10-05 14:49:49 +0900

    버튼 삭제

--- src/App.js
+++ src/App.js
@@ -7,16 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          안녕
+          안녕 하세요
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
