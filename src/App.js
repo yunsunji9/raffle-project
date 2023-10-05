72cbb64d1e470f3b8e56d8bdcf642c9b9de8dcbe
yunseonji <amy@shoeprize.com>
2023-10-05 14:56:04 +0900

    index텍스트 변경

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          안녕하세요 윤선지입니다.
         </p>
         <a
           className="App-link"
